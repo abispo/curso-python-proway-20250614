5ab40657a362c9434fb0b3a764818beda5ae9429
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-06-14 17:00:16 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20250614-aula01/prog07.py
@@ -0,0 +1,27 @@
+"""
+Operadores em Python
+
+Operadores de comparação.
+
+Operadores de comparação, como o próprio nome já diz, são operadores que irão comparar valores. Essa comparação sempre irá retornar um valor booleano (True ou False)
+"""
+
+if __name__ == "__main__":
+
+    # Maior que
+    print(50 > 40)      # True
+
+    # Maior ou igual a
+    print(13 >= 13)     # True
+
+    # Menor que
+    print(4 < 2)        # False
+
+    # Menor ou igual a
+    print(34 <= 27)     # False
+
+    # Igual a
+    print(10 == 10)     # True
+
+    # Diferente de
+    print(45 != 45)     # False
