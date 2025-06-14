aa7e34cbd4c241d9d19c00e6eb3f4db061904c15
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-06-14 17:53:15 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20250614-aula01/ex01.py
@@ -0,0 +1,16 @@
+"""
+Crie um programa que peça ao usuário para digitar um número inteiro e exiba se ele é positivo, negativo ou zero.
+"""
+
+if __name__ == "__main__":
+
+    numero = int(input("Informe um número: "))
+
+    if numero < 0:
+        print(f"O número {numero} é negativo.")
+
+    elif numero > 0:
+        print(f"O número {numero} é positivo.")
+
+    else:
+        print("Zero")
