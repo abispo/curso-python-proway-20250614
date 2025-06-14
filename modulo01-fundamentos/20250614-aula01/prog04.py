3b1625c76214e993dcf40a607f2da94e0dff309e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-06-14 16:36:44 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20250614-aula01/prog04.py
@@ -0,0 +1,15 @@
+"""
+Tipos de dados em Python
+
+Tipo booleano.
+
+Um boolean é basicamente um tipo que pode ter no máximo 2 valores: Ou verdadeiro, ou falso
+
+"""
+
+if __name__ == "__main__":
+
+    print(10 > 5)
+    print(4 == 3 and 5 < 6)
+    print(bool(0))
+    print(bool(-3454))
