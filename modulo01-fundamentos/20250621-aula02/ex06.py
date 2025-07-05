f76980467478ca4a35b0f411e588379737f14bef
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-07-05 15:01:43 -0300

    ex06

---
+++ modulo01-fundamentos/20250621-aula02/ex06.py
@@ -0,0 +1,15 @@
+"""
+6. Escreva um programa que imprima o seguinte padrão no terminal:
+```
+#
+##
+###
+####
+#####
+```
+"""
+
+if __name__ == "__main__":
+
+    for numero in range(1, 11):
+        print('#'*numero)
