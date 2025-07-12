107c72f7f8c897d4f475a32be0e67c95242e651d
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-07-12 14:10:48 -0300

    Arquivos aula 03 python fundamentos

--- modulo01-fundamentos/20250705-aula03/prog04.py
+++ modulo01-fundamentos/20250705-aula03/prog04.py
@@ -8,9 +8,9 @@ No Python, é possível criarmos funções que recebem uma quantidade arbitrári
 def calculo_media_leitura(*args) -> float:
     return sum(args) / len(args)
 
-
+# Utilizamos o padrão **kwargs para indicar que uma função pode receber uma quantidade indeterminada de valores informando o nome dos parâmetros
 def mostra_info(**kwargs):
-    for chave, valor in kwargs:
+    for chave, valor in kwargs.items():
         print(f"{chave}: {valor}")
 
     print('*'*30)
@@ -21,3 +21,8 @@ if __name__ == "__main__":
     print(calculo_media_leitura(0.6, 0.7, 0.5, 0.9))
     print(calculo_media_leitura(0.6, 0.7))
     print(calculo_media_leitura(0.6, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 1))
+    print(calculo_media_leitura(*(0.4, 0.6, 0.6,)))
+
+    # Passamos os valores para a função indicando o nome dos parâmetros
+    mostra_info(nome="Maria", estado="SP", cidade="Marília", estado_civil="Solteira")
+    mostra_info(**{"nome": "João", "idade": 30, "cidade": "Indaial"})
