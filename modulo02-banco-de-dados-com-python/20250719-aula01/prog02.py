895248b63583921a38edc7b05161d57fec782db6
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-07-19 17:16:08 -0300

    Arquivos aula 01 python com banco de dados

--- modulo02-banco-de-dados-com-python/20250719-aula01/prog02.py
+++ modulo02-banco-de-dados-com-python/20250719-aula01/prog02.py
@@ -1,15 +1,22 @@
 # https://api.coinlore.net/api
 
+import os
+
 import pymysql
 import pymysql.cursors
 import requests
 
+from dotenv import load_dotenv
+
+# Essa função carrega os pares chave-valor do arquivo .env e os transforma em variáveis de ambiente e seus valores
+load_dotenv()
+
 if __name__ == "__main__":
 
     conexao = pymysql.connect(
-        user="root",
-        password="admin",
-        host="127.0.0.1",
-        port=3306,
-        database="criptomoedas"
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        host=os.getenv("DATABASE_HOST"),
+        port=int(os.getenv("DATABASE_PORT")),
+        database=os.getenv("DATABASE_NAME")
     )
