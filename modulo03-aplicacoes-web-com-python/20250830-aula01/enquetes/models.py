b7b7261a1552ca2d8ca9fb288d06e3e1245e588e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-08-30 17:43:39 -0300

    Arquivos aula 01 aplicações web com python

--- modulo03-aplicacoes-web-com-python/20250830-aula01/enquetes/models.py
+++ modulo03-aplicacoes-web-com-python/20250830-aula01/enquetes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Pergunta(models.Model):
+    texto_pergunta = models.CharField(max_length=100)
+    data_publicacao = models.DateTimeField("Data de publicação")
+
+    class Meta:
+        db_table = "perguntas"
+
+    def __str__(self):
+        return self.texto_pergunta
+    
+    def publicado_recentemente(self):
+        return self.data_publicacao >= timezone.now() - timezone.timedelta(days=1)
+
+class Opcao(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto_opcao = models.CharField(max_length=100)
+    votos = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "opcoes"
