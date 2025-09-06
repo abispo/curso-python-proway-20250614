abaebf101fbabfb304eadbc300a828e97f93e7e1
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-06 16:14:11 -0300

    Arquivos aula 02 aplicações web com Python

--- modulo03-aplicacoes-web-com-python/20250906-aula02/enquetes/views.py
+++ modulo03-aplicacoes-web-com-python/20250906-aula02/enquetes/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Pergunta
 
 def index(request):
     ultimas_cinco_perguntas = Pergunta.objects.order_by("-data_publicacao")[:5]
-    saida = ", ".join([pergunta.texto_pergunta for pergunta in ultimas_cinco_perguntas])
+    contexto = {"ultimas_cinco_perguntas": ultimas_cinco_perguntas}
 
-    return HttpResponse(saida)
+    return render(request, "enquetes/index.html", contexto)
 
 
 def detalhes(request, pergunta_id):
-    return HttpResponse(f"Você está na página de detalhes da pergunta '{pergunta_id}'.")
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    return render(
+        request,
+        "enquetes/detalhes.html",
+        context={"pergunta": pergunta}
+    )
 
 
 def resultados(request, pergunta_id):
