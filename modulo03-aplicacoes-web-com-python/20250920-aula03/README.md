2d64d8df333c031ed3339ac7a373e91cc69bc697
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-20 13:41:14 -0300

    Arquivos aula 03 aplicações web com Python

---
+++ modulo03-aplicacoes-web-com-python/20250920-aula03/README.md
@@ -0,0 +1,10 @@
+# Desafio
+
+Você irá criar um novo pacote, chamado `estatisticas`. Esse pacote irá ter uma página que mostrará algumas informações sobre as perguntas cadastradas no sistema. Esse pacote terá apenas 1 rota, que será a rota `index`. A página que será exibida, deve mostrar as seguintes informações:
+
+* Quantidade de perguntas cadastradas
+* Quantidade de opções cadastradas
+* Média de opções por pergunta
+* Lista das 3 perguntas com mais votos, ordenada pela quantidade de votos.
+
+O link para a página de estatísticas deve ser mostrado na página principal do pacote enquetes. O texto fica a sua escolha.
---
+++ modulo03-aplicacoes-web-com-python/20250920-aula03/enquetes/views.py
@@ -0,0 +1,62 @@
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Pergunta, Opcao
+
+def index(request):
+    ultimas_cinco_perguntas = Pergunta.objects.order_by("-data_publicacao")[:5]
+    contexto = {"ultimas_cinco_perguntas": ultimas_cinco_perguntas}
+
+    return render(request, "enquetes/index.html", contexto)
+
+
+def detalhes(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    return render(
+        request,
+        "enquetes/detalhes.html",
+        context={"pergunta": pergunta}
+    )
+
+
+def resultados(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    return render(
+        request,
+        "enquetes/resultados.html",
+        {"pergunta": pergunta}
+    )
+
+
+def votar(request, pergunta_id):
+    
+    # A função get_object_or_404 retorna a instância do objeto se existir, se não, lança o erro 404
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    try:
+        # Na linha abaixo vamos tentar carregar a opção a partir do valor que está armazenado no atributo POST do objeto request. Caso não exista a chave opcao, ou essa opcao não exista na tabela, será gerada uma exceção
+        opcao_selecionada = pergunta.opcao_set.get(pk=request.POST["opcao"])
+    except (KeyError, Opcao.DoesNotExist):
+        # O template é novamente renderizado, porém com uma mensagem de erro na tela.
+        return render(
+            request,
+            "enquetes/detalhes.html",
+            {
+                "pergunta": pergunta,
+                "mensagem_erro": "Você deve selecionar uma opção"
+            }
+        )
+    
+    else:
+        # Abaixo incrementamos o valor do atributo votos da opcao em 1, e depois salvamos os dados na tabela
+        opcao_selecionada.votos = F("votos") + 1
+        opcao_selecionada.save()
+
+        # Após receber uma requisição do tipo POST, temos que utilizar o redirect para redicionar o usuário para outra página. A função reverse vai gerar a URL de acordo com o namespace e o nome, enquando o parâmetro args é utilizado quando a rota possui algum parâmetro.
+        return HttpResponseRedirect(
+            reverse("enquetes:resultados", args=(pergunta.id,))
+        )
