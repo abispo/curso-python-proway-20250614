a070b904ac46bbdf9b56a951f13dfb9520df8334
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-09-27 16:18:19 -0300

    Arquivos aula 04 aplicações web com Python

--- modulo03-aplicacoes-web-com-python/20250927-aula04/helpdesk/settings.py
+++ modulo03-aplicacoes-web-com-python/20250927-aula04/helpdesk/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'helpdesk.urls'
@@ -129,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "core.User"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
