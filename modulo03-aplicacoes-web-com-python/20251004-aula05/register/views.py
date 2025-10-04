b5cad22581a32f38dc131cada0ced7b34a7f0309
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-10-04 18:12:23 -0300

    Arquivos aula 05 aplicações web com Python

--- modulo03-aplicacoes-web-com-python/20251004-aula05/register/views.py
+++ modulo03-aplicacoes-web-com-python/20251004-aula05/register/views.py
@@ -15,9 +15,11 @@ def pre_register(request):
         )
     
     elif request.method == "POST":
-        # Verifica se o e-mail já não está no pré-registro e é válido
+        # Pegar o email dessa maneira: request.POST["email"]
         # Verifica se o e-mail já não está cadastrado na tabela de usuários (users)
+        # Verifica se o e-mail já não está no pré-registro e é válido
         # Se alguma das situações acima ocorrer, renderizar novamente a página enviando uma mensagem de erro
         # Se não houverem erros
         # Salvar o registro na tabela pre_register
         # Enviar o e-mail para o usuario (desafio)
+        pass
