e5683f9fdcf51f668cc43e994a1b370c089181e5
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-10-11 14:32:29 -0300

    Arquivos aula 06 aplicações web com Python

---
+++ modulo03-aplicacoes-web-com-python/20251011-aula06/register/validators.py
@@ -0,0 +1,11 @@
+from core.models import User
+
+from .models import PreRegister
+
+def email_already_exists_in_users(email: str) -> bool:
+    return User.objects.filter(email=email).exists()
+
+def email_alreads_exists_in_pre_register(email: str) -> bool:
+    return PreRegister.objects.filter(
+        email=email, is_valid=True
+    ).exists()
--- modulo03-aplicacoes-web-com-python/20251011-aula06/register/views.py
+++ modulo03-aplicacoes-web-com-python/20251011-aula06/register/views.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.shortcuts import render
 
 from .forms import PreRegisterForm
+from .validators import email_already_exists_in_users, email_alreads_exists_in_pre_register
 
 def pre_register(request):
 
@@ -16,10 +18,32 @@ def pre_register(request):
     
     elif request.method == "POST":
         # Pegar o email dessa maneira: request.POST["email"]
-        # Verifica se o e-mail já não está cadastrado na tabela de usuários (users)
+
         # Verifica se o e-mail já não está no pré-registro e é válido
         # Se alguma das situações acima ocorrer, renderizar novamente a página enviando uma mensagem de erro
         # Se não houverem erros
         # Salvar o registro na tabela pre_register
         # Enviar o e-mail para o usuario (desafio)
-        pass
+
+        try:
+            form = PreRegisterForm(request.POST)
+
+            if form.is_valid():
+                email = form.cleaned_data["email"]
+                # Verifica se o e-mail já não está cadastrado na tabela de usuários (users)
+            
+                if email_already_exists_in_users(email):
+                    form.add_error("email", "O e-mail informado já está cadastrado no sistema.")
+
+                if email_alreads_exists_in_pre_register(email):
+                    form.add_error("email", "O e-mail informado já está em processo de pré-cadastro. Verifique seu e-mail.")
+
+                if not form.is_valid():
+                    return render(
+                        request,
+                        "register/pre_register.html",
+                        {"form": form}
+                    )
+                
+        except Exception as exc:
+            print(f"Erro ao receber os dados do formulário: {exc}")
