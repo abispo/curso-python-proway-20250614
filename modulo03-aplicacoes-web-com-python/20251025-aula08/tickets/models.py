b0015a716d4a911f6d769419b1f4b94cbd9ecb9f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-10-25 16:39:14 -0300

    Criada model tickets

--- modulo03-aplicacoes-web-com-python/20251025-aula08/tickets/models.py
+++ modulo03-aplicacoes-web-com-python/20251025-aula08/tickets/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+
+    STATUS_CHOICES = [
+        ("open", "Aberto",),
+        ("in_progress", "Em andamento",),
+        ("closed", "Fechado",),
+        ("cancelled", "Cancelado",),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    description_resolved = models.TextField()
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="open")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "tickets"
