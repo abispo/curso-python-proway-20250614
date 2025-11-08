df0b98b9c59f4079e41b15af3b681d0c9afb6a7b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-11-08 17:43:15 -0300

    Alteração do admin do django

--- modulo03-aplicacoes-web-com-python/20251108-aula10/tickets/admin.py
+++ modulo03-aplicacoes-web-com-python/20251108-aula10/tickets/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import Ticket
 
-admin.site.register(Ticket)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "status", "created_at")
+    list_filter = ("status", "owner",)
+    search_fields = ("title", "description",)
+    ordering = ("-created_at",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+
+        if request.user.groups.filter(name="suporte").exists():
+            return queryset
+        
+        return queryset.filter(owner=request.user)
